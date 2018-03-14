5688f699f3addedd87839d10a4d09636efc78eb4
Roman Zhuzha <roman.zhuzha@trivago.com>
2018-03-14 04:33:06 +0100

    increase consistency
    use $HOME instead of `~`

--- completions/asp.fish
+++ completions/asp.fish
@@ -1,5 +1,5 @@
 function __fish_print_aws_profiles -d 'Prints a list of AWS profiles' -a select
-  command sed -n "s/^\[\(profile[[:space:]]*\)*\(.*\)\]/\2/p" ~/.aws/config ~/.aws/credentials | sort | uniq
+  command sed -n "s/^\[\(profile[[:space:]]*\)*\(.*\)\]/\2/p" "$HOME/.aws/config" "$HOME/.aws/credentials" | sort | uniq
 end
 
 complete -c asp -f -a "(__fish_print_aws_profiles)"
