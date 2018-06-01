a915c4dcad63467a8c1c57bd648f936139fe5f60
Michael Goodness <mgoodness@gmail.com>
2018-06-01 10:04:50 -0500

    Update completions

--- completions/asp.fish
+++ completions/asp.fish
@@ -1,5 +1,5 @@
 function __fish_print_aws_profiles -d 'Prints a list of AWS profiles' -a select
-  command sed -n "s/^\[\(.*\)\]/\1/p" ~/.aws/credentials
+  command sed -n 's/^\[\(.*\)\]/\1/p' ~/.aws/credentials
 end
 
-complete -c asp -f -a "(__fish_print_aws_profiles)"
+complete -xc 'asp' -a '(__fish_print_aws_profiles)'
